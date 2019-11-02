591bf2a95339104c39800bc7d268f63bfd41940f
CallumCoy <callumcoy@knights.ucf.edu>
2019-11-02 12:31:16 -0700

    Create GetPeople.php
    gets students or members

---
+++ SitePages/PHPScript/GetPeople.php
@@ -0,0 +1,48 @@
+<?php
+    if(isset($_GET["type"])){
+        $people = $_GET["type"];
+    } else {
+        $people = "members";
+    }
+
+    $query = "SELECT USERID
+              FROM " . $people . "
+              WHERE RSO_ID = ?";
+
+    if($stmt = $link->prepare($query)){
+        $stmt->bind_param("i", $rID);
+        $stmt->execute();
+        $stmt->bind_result($ID);
+        $stmt->store_result();
+
+
+        while($stmt->fetch()){
+
+            $query = "SELECT Username, ProfilePic
+                      FROM Users
+                      WHERE UserID = ?";
+
+            if($stmt2 = $link->prepare($query)){
+                $stmt2->bind_param("i", $ID);
+                $stmt2->execute();
+                $stmt2->bind_result($Name, $Pic);
+                $stmt2->store_result();
+                $stmt2->fetch();
+
+                echo ('<div class="person">
+                            <div class="miniPic">
+                                    <img src="' . $Pic . '" alt="Space" class="eventPic">
+                                </div>                      
+                                <div class="InfoTable">
+                                    <div class="Info">
+                                        ' . $Name . '
+                                    </div>
+                                </div>
+                        </div>');
+                
+                $stmt2->close();
+            }
+        }
+    $stmt->close();
+    }
+?>
