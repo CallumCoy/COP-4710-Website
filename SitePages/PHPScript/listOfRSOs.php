589af66d56566f2c597f207228fc332ff0256b92
CallumCoy <callumcoy@knights.ucf.edu>
2019-11-02 12:34:07 -0700

    Update listOfRSOs.php
    No longer start sessions in scripts, also school ID is now a session variable so getting rid of obselete code.

--- SitePages/PHPScript/listOfRSOs.php
+++ SitePages/PHPScript/listOfRSOs.php
@@ -1,39 +1,26 @@
 <?php
+    error_log("hi");
     echo '<datalist id=RSOs>';
 
     $error = "";
 
     require_once "config.php";
-    session_start();
-
-    # get the id of the school the user is attending
-    $query = '  SELECT SchoolID
-                FROM students
-                WHERE UserID = ?';
-
-    if($stmt = $link->prepare($query)){
-
-        $stmt->bind_param($_SESSION['id']);
-        $stmt->execute();
-        $stmt->bind_result($Sid);
-    }
-
-    $stmt->close();
-
     $query = '  SELECT RSO_Name
                 FROM rso
                 WHERE SchoolID = ?';
 
-    if($stmt = $link->prepare($query)){
+    if($stmt2 = $link->prepare($query)){
 
-        $stmt->bind_param($Sid);
-        $stmt->execute();
-        $stmt->bind_result($club);
+        $stmt2->bind_param("i", $_SESSION["sid"]);
+        $stmt2->execute();
+        $stmt2->bind_result($club);
 
         while($stmt2->fetch()){
             echo '<option value="' . $club . '">';
         }
+        $stmt2->close();
     }
+
 
     echo '</datalist>';
 ?>
