d08fb0385abcff1c1f3cf2d399af38465c0663ea
CallumCoy <callumcoy@knights.ucf.edu>
2019-11-09 09:35:03 -0800

    Create BecomeStudent.php
    updates things when you become a Student.

---
+++ SitePages/PHPScript/BecomeStudent.php
@@ -0,0 +1,24 @@
+<?php
+    $query =   'SELECT 1
+                FROM student
+                WHERE UserID = ?';
+
+    if($stmt = $link->prepare($query)){
+        $stmt->bind_param('i', $uid);
+        $stmt->execute();
+        $stmt->store_result();
+
+        if($stmt->num_rows() == 0) {
+
+            $query =   'INSERT INTO students
+                        VALUES (?, ?)';
+            
+            if($signUp = $link->prepare($query)){
+                $signUp->bind_param('ii', $uid, $sid);
+                $signUp->execute();
+                $signUp->close();
+            }
+        }
+        $stmt->close();
+    }
+?>
