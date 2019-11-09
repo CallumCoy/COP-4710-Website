6041f05f0e34f38ab18ac2b7c75b62409df84c70
CallumCoy <callumcoy@knights.ucf.edu>
2019-11-09 09:40:00 -0800

    Update navBar.php
    changes the navBar based one super admin status

--- SitePages/PHPScript/navBar.php
+++ SitePages/PHPScript/navBar.php
@@ -2,9 +2,15 @@
     echo '<ul class="topBar"> 
     <li><a href="../index.php">Home</a></li>
     <li><a href="../PHPPage/EventListPage.php">Events</a></li>
-    <li><a href="../PHPPage/LoadMyRSO.php">RSC</a></li>
-    <li><a href="../PHPPage/LoadSchool.php">School</a></li>
-    ';
+    <li><a href="../PHPPage/LoadMyRSO.php">RSC</a></li>';
+
+    include 'AreTheyAdmin.php';
+
+    if($admin > 1){
+        echo '<li><a href="../PHPPage/SchoolList.php">School</a></li>';
+    } else if(isset($_SESSION['sid'])) {
+        echo '<li><a href="../PHPPage/LoadSchool.php">School</a></li>';
+    }
 
     if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
         echo '<li class="right"><a href="#"> Profile </a></li>
