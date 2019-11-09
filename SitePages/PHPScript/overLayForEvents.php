cccaff5e8e87f4bd855ae2ce7dd2c0bf031ab582
CallumCoy <callumcoy@knights.ucf.edu>
2019-11-09 09:41:54 -0800

    Update overLayForEvents.php
    added the mega filter to the event overlay

--- SitePages/PHPScript/overLayForEvents.php
+++ SitePages/PHPScript/overLayForEvents.php
@@ -8,16 +8,6 @@
 
         #Adds some options if the user is logged in
         if (isset($_SESSION["loggedin"]) && ($_SESSION["loggedin"] === true)) {
-            
-            echo '<form action="EventListPage.php" method="get">
-                    <input list="Events" name="Name" class="Name">';
-                        include "ListOfEvents.php";
-            echo '  <button type="submit" class="EventSearch"> GO </button>
-                  </form>';
-
-
-            #shows an extra option if the user is an admin of any RSOs
-            require_once "config.php";
             $sql = "SELECT *
                      FROM events
                      WHERE HostingUserID = ?";
@@ -32,19 +22,47 @@
                     mysqli_stmt_store_result($stmt);
 
                     if (mysqli_stmt_num_rows($stmt) > 0){
-                        echo '<a href="/PHPPage/EventListPage.php?UHost=' . $_SESSION['id'] . '">Edit My Events</a>';
+                        echo '<a href="/PHPPage/EventListPage.php?UHost=' . $_SESSION['id'] . '&view=0">Edit My Events</a>';
                     }
                 }
                 mysqli_stmt_close($stmt);
             }
 
             if($_SESSION != NULL){
-                echo '<a href="/PHPPage/LoadMyEvents.php?level=Viewer">View My Events</a>';
+                echo '<a href="/PHPPage/EventListPage.php?UHost=' . $_SESSION['id'] . '&view=1">View My Events</a>';
             }
+
+            echo '<a href="/PHPPage/NewEvent.php">Make a Events</a>
+                    <br> 
+                    <br> Search By Event Name
+                    <form action="EventListPage.php" method="get">
+                    <input list="Events" name="Name" class="Name overLap">';
+                        include "ListOfEvents.php";
+            echo '  <br>
+                    <br> Hosting RSO
+                    <select list="RSOsid" name="Host" class="RSOs overLap">';
+                        include "listOfRSOsID.php";
+            echo '  </select>
+                    <br> 
+                    <br> Hosting School </br>
+                    <select list="Schools" name="School" class="RSOs overLap">';
+                        include "ListOfSchools.php";
+            echo '  </select>
+                    <br> 
+                    <br> Start Time </br>
+                    <input type="date" name="StartDate" class="overLap">
+                    <br> 
+                    <br>
+                    <input type="time" name="StartTime" class="overLap">  
+                    <br> 
+                    <br>
+                    Show Past Events <input type="checkbox" name="Old" class="overLap">  
+                    <br> 
+                    <br>
+                    <button type="submit" class="RSOSearch"> Search </button>
+                    </form>';
         }
 
-            echo '<a href="#">Clients</a>
-            <a href="#">Contact</a>
-           </div>
-        </div>';
+            echo    '</div>
+            </div>';
 ?>
