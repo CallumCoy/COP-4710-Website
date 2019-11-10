d7b2e0a14165d4a54864ad8b9e5c8811953cd821
CallumCoy <callumcoy@knights.ucf.edu>
2019-11-10 14:52:56 -0800

    Update NewEvent.php
    added date select to NewEvent

--- SitePages/PHPPage/NewEvent.php
+++ SitePages/PHPPage/NewEvent.php
@@ -138,6 +138,37 @@ include_once '../PHPScript/GetEventFromID.php';
                     
                 ?>
 
+                
+                <div class="section"> Start Date: </div>
+                <div class="inputSec"> <input type="date" name="StartDate" id="StartDate" required> </div>
+                
+                <div class="section"> Start Time: </div><div class="inputSec"></div>
+                
+                <select name="StartHours" required>
+                        <option value="01">01</option>
+                        <option value="02">02</option>
+                        <option value="03">03</option>
+                        <option value="04">04</option>
+                        <option value="05">05</option>
+                        <option value="06">06</option>
+                        <option value="07">07</option>
+                        <option value="08">08</option>
+                        <option value="09">09</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12" selected>12</option>
+                    </select>
+                    <select name="StartMinute" id="" required>
+                        <option value="00" selected>00</option>
+                        <option value="15">15</option>
+                        <option value="30">30</option>
+                        <option value="45">45</option>
+                    </select>
+                    <select name="StartClockSide" required>
+                        <option value="AM">AM</option>
+                        <option value="PM" selected>PM</option>
+                    </select>
+
                 <div class="section"> Location for Event </div>
                 <div class="break"></div>
                 <div class="section"> </div>
