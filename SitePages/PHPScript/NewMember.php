96e3e2d6862f058f1f3b657537e10dfcb0491646
CallumCoy <callumcoy@knights.ucf.edu>
2019-11-10 14:58:00 -0800

    Update NewMember.php
    moved the counter

--- SitePages/PHPScript/NewMember.php
+++ SitePages/PHPScript/NewMember.php
@@ -1,23 +1,20 @@
 <?php
 
-    if(strpos($email,$emailExt)){
-        $query =   'UPDATE members
-                    SET RSO_ID = ' . $rid . '
-                    WHERE UserID = ' . $uid;
+    $query =   'INSERT INTO members
+                VALUES (' . $rid . ', ' . $_SESSION['id'] . ')';
+                
+    if ($update = $link->prepare($query)){
+        $update->execute();
+        $update->close();
+
+        $query =   'UPDATE rso
+                    SET NumofMembers = NumofMembers + 1
+                    WHERE RSO_ID = ' . $rid;
                     
-        if ($update = $link->prepare($query)){
+        if($update = $link->prepare($query)){
             $update->execute();
             $update->close();
-
-            $query =   'UPDATE rso
-                        SET NumofMembers = NumOfMembers + 1
-                        WHERE RSO_ID = ' . $rid;
-                        
-            if($update = $link->prepare($query)){
-                $update->execute();
-                $update->close();
-        
-            }
+    
         }
     }
 ?>
