b1c0a756f0f878b1aff9a17d980c3f93cc94012a
CallumCoy <callumcoy@knights.ucf.edu>
2019-11-10 15:00:05 -0800

    Update SetCookies.php
    cookies now updates your education

--- SitePages/PHPScript/SetCookies.php
+++ SitePages/PHPScript/SetCookies.php
@@ -4,5 +4,22 @@
     if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
         $userID = $_SESSION["id"];
         setcookie("UserNumber", $userID, time()+3600, "/");
+        if(!isset($_SESSION['sid'])){
+            $query =   'SELECT SchoolID
+                        FROM users
+                        WHERE UserID = ?';
+
+            if($stmt = $link->prepare($query)){
+                $stmt->bind_param('i', $_SESSION['id']);
+                $stmt->execute();
+                $stmt->bind_result($sid);
+                $stmt->store_result();
+                $stmt->fetch();
+
+                if($stmt->num_rows == 1){
+                    $_SESSION['sid'] = $sid;
+                }
+            }
+        }
     }
 ?>
