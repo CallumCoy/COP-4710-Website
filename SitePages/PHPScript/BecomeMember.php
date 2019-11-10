181f8cb2395a28a146adf910847c12227838b09e
CallumCoy <callumcoy@knights.ucf.edu>
2019-11-10 14:55:31 -0800

    Update BecomeMember.php
    fixing some small typos

--- SitePages/PHPScript/BecomeMember.php
+++ SitePages/PHPScript/BecomeMember.php
@@ -1,4 +1,15 @@
 <?php
+    session_start();
+    require_once '../PHPScript/config.php';
+    include_once '../PHPScript/IsLoggedIn.php';
+    include_once '../PHPScript/SetCookies.php';
+    
+    if(!isset($_GET['rid'])){
+        header("location: ../index.php");
+    } else {
+        $rid = $_GET['rid'];
+    }
+
     $query =   'SELECT SchoolID
                 FROM rso
                 WHERE RSO_ID = ?';
@@ -12,15 +23,20 @@
 
         if($school == $_SESSION['sid']) {
 
-            $query =   'INSERT INTO memebrs
+            $query =   'INSERT INTO members
                         VALUES (?, ?)';
             
             if($signUp = $link->prepare($query)){
-                $signUp->bind_param('ii', $uid, $rid);
+                $signUp->bind_param('ii', $_SESSION['id'], $rid);
                 $signUp->execute();
                 $signUp->close();
+
+                include "../PHPScript/NewMember.php";
             }
         }
         $stmt->close();
+
+    header("location: ../PHPPage/LoadRSOPage.php?RSO=$rid");
+
     }
 ?>
