73522cd3fcd5cd9131a7de2380049b6b346d7b4d
CallumCoy <callumcoy@knights.ucf.edu>
2019-11-10 15:00:58 -0800

    Update UpdateEvent.php
    not sure what heappened but everyhting broke in here, now is fixed

--- SitePages/PHPScript/UpdateEvent.php
+++ SitePages/PHPScript/UpdateEvent.php
@@ -5,11 +5,15 @@
     $uid = $rid = $name = $desc = $pic = $Lat = $Long = $Building = $Floor = $Room = $eid = $error = "";
 
     $eid = trim($_GET["eid"]);
-    $uid = trim($_GET["uid"]);
+    $uid = trim($_SESSION["id"]);
     $rid = trim($_GET["rid"]);
     $name = trim($_GET["Name"]);
     $desc = trim($_GET["Desc"]);
     $pic = trim($_GET["myPhoto"]);
+    $date = trim($_GET["StartDate"]);
+    $hour = trim($_GET["StartHours"]);
+    $min = trim($_GET["StartMinute"]);
+    $clockCycle = trim($_GET["StartClockSide"]);
     $Lat = trim($_GET["Lat"]);
     $Long = trim($_GET["Long"]);
     $Building = trim($_GET["Building"]);
@@ -23,91 +27,116 @@
         }
     }
 
+    $hour = $hour%12;
+
+    if(($min % 15) != 0 || $min > 59 || $min < 0){
+        $temp = $min/60;
+        $temp2 = $min%15;
+        $min = $min - ($temp * 60) - $temp2; 
+    }
+
+    $lilTime = date("H:i:s", strtoTime("$hour:$min $clockCycle"));
+    error_log($lilTime);
+    $time = date('Y-m-d H:i:s', strtoTime("$date $lilTime"));
+    error_log($time);
+
     require_once "config.php";
     include '../PHPScript/AreTheyAdmin.php';
+    error_log("-[==-=========================================================================================== ");
+    require '../PHPScript/UpdateLocation.php';
 
-    #does this data already exist?
+    $query =   'SELECT 1
+                FROM events
+                WHERE MainLocationID = ? && Start_Time = ?';
 
-
-
-    if($eid != 0){
-        error_Log("1 row");
-
-        include_once '../PHPScript/CheckPriosEvents.php';
-
-        $query = "  SELECT approved
-                    FROM events
-                    WHERE EventID = ?";
+    if($check = $link->prepare($query)){
         
+        $check->bind_param('is', $lid, $time);
+        $check->execute();
+        $check->store_result();
 
-        if($stmt = $link->prepare($query)){
-            $stmt->bind_param("i", $admin);
-            error_log("file updated? Error: " . $pic);
-            error_log("file updated? Error: " . $desc);
-            $stmt->execute();
-            $stmt->bind_result($admin);
-            $stmt->close();
-        }
+        if($check->num_rows == 0){
+            if($eid != 0){
+                error_Log("1 row");
 
-        $query = "  UPDATE events
-                    SET EventName = ?, Start_Time = ?, EventPic = ?, EventDesc = ?, InviteType = ?, HostingUserID = ?, Host_RSO_ID = ?, approved = ?
-                    WHERE EventID = ?";
-            error_log("file updated? Error: " . $query);
-
-        if($admin > 0){
-            $admin = 1;
-        }
-
-        if($stmt2 = $link->prepare($query)){
-            $stmt2->bind_param("ssssiiiii", $name, $time, $pic, $desc, $invType, $uid ,$rid, $admin, $eid);
-            error_log("file updated? Error: " . $pic);
-            error_log("file updated? Error: " . $desc);
-            $stmt2->execute();
-
-            $stmt2->close();
-        }
-
-    } elseif ($eid == 0){
-                            
-        $query = "  SELECT 1
-                    FROM events
-                    WHERE EventName = ? && Host_RSO_ID = ? && SchoolID = ?";
-    
-        if($stmt1 = $link->prepare($query)){
-            $stmt1->bind_param("sii", $name, $rid, $_SESSION["sid"]);
-            $stmt1->execute();
-
-            error_log("RSO created? Error: " . $rid);
-
-            $stmt1->store_result();
-            $stmt1->fetch();
-
-            error_log("RSO created? Error: " . $stmt2->error);
-            error_log("RSO created? Error: " . $rid);
-
-            if($stmt1->num_rows < 1){
-                $query =   "INSERT INTO events
-                            VALUES (default, ?,?,?, 1,?,?,?,?,?,?)";
+                $query = "  SELECT approved
+                            FROM events
+                            WHERE EventID = ?";
                 
-                error_Log("0 rows");
+
+                if($stmt = $link->prepare($query)){
+                    $stmt->bind_param("i", $admin);
+                    error_log("location: " . $lid);
+                    error_log("file updated? Error: " . $desc);
+                    $stmt->execute();
+                    $stmt->bind_result($admin);
+                    $stmt->close();
+                }
+
+                $query = "  UPDATE events
+                            SET EventName = ?, Start_Time = ?, EventPic = ?, EventDesc = ?, InviteType = ?, HostingUserID = ?, Host_RSO_ID = ?, approved = ?, MainLocationID = ?
+                            WHERE EventID = ?";
+                    error_log("file updated? Error: " . $query);
+                    error_log("file updated? admin level: " . $admin);
+                    error_log("file updated? admin level: " . $lid);
+
+                if($admin > 0){
+                    $admin = 1;
+                }
+
                 if($stmt2 = $link->prepare($query)){
-                    error_Log($_SESSION["sid"] . " " . $_SESSION["id"]);
-                    $time = "2019-11-14 07:00:00";
-                    $stmt2->bind_param("ssssiiiii", $name, $time, $pic, $desc, $invType, $uid ,$rid, $_SESSION['sid'], $admin);
+                    $stmt2->bind_param("ssssiiiiii", $name, $time, $pic, $desc, $invType, $uid ,$rid, $admin, $lid, $eid);
+                    error_log("file updated? Error: " . $pic);
                     $stmt2->execute();
-                    error_log("RSO created? Error: " . $stmt2->error);
-                    error_log("RSO created? Error: " . $name . ' ' . $time . ' ' . $pic . ' ' . $desc . ' ' . $invType . ' ' . $uid . ' ' . $rid . ' ' . $eid . ' ' . $admin);
+                    error_log("file updated? Error: " . $stmt2->error);
 
                     $stmt2->close();
                 }
 
+            } elseif ($eid == 0){
+                                    
+                $query = "  SELECT 1
+                            FROM events
+                            WHERE EventName = ? && Host_RSO_ID = ? && SchoolID = ?";
+            
+                if($stmt1 = $link->prepare($query)){
+                    $stmt1->bind_param("sii", $name, $rid, $_SESSION["sid"]);
+                    $stmt1->execute();
+
+                    error_log("RSO created? Error: " . $rid);
+
+                    $stmt1->store_result();
+                    $stmt1->fetch();
+
+                    error_log("RSO created? Error: " . $stmt2->error);
+                    error_log("RSO created? Error: " . $rid);
+
+                    if($stmt1->num_rows < 1){
+                        $query =   "INSERT INTO events
+                                    VALUES (default, ?,?,?,?,?,?,?,?,?,?)";
+                        
+                        error_Log("0 rows");
+                        if($stmt2 = $link->prepare($query)){
+                            error_Log($_SESSION["sid"] . " " . $_SESSION["id"]);
+                            $time = "2019-11-14 07:00:00";
+                            $stmt2->bind_param("sssisiiiii", $name, $time, $pic, $lid, $desc, $invType, $uid ,$rid, $_SESSION['sid'], $admin);
+                            $stmt2->execute();
+                            error_log("RSO created? Error: " . $stmt2->error);
+                            error_log("RSO created? Error: " . $name . ' ' . $time . ' ' . $pic . ' ' . $desc . ' ' . $invType . ' ' . $uid . ' ' . $rid . ' ' . $eid . ' ' . $admin);
+
+                            $stmt2->close();
+                        }
+
+                    }
+                }
+
+            } else {
+                $error = "something really bad happened somewhere";
             }
         }
-
-    } else {
-        $error = "something really bad happened somewhere";
+        $check->close();
     }
-    
+
     error_log("file updated? Error: " . $link->error);
     
                             
@@ -128,6 +157,6 @@
         header("location: /../PHPPage/LoadEventPage.php?Event=". $eid);
         $stmt1->close();
     }
-    error_log("file updated? Error: " . $link->error);
+    error_log("file updated? Error: $link->error $query");
     $link->close();
 ?>
