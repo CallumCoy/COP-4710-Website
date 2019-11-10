d0809550729e8336382c193f97d84cc6e60175d4
CallumCoy <callumcoy@knights.ucf.edu>
2019-11-10 14:56:17 -0800

    Update GetEventFromID.php
    date overload

--- SitePages/PHPScript/GetEventFromID.php
+++ SitePages/PHPScript/GetEventFromID.php
@@ -10,6 +10,23 @@
         $stmt->store_result();
         $stmt->fetch();
 
+        $eDate = date("Y-m-d", strtoTime("$eStart"));
+        $eHour = date("h", strtoTime("$eStart"));
+        $eMin = date("m", strtoTime("$eStart"));
+        $eSide = date("A", strtoTime("$eStart"));
+        
+        $query =   "SELECT *
+                    FROM locations
+                    WHERE locID = ?";
+
+        if($stmt = $link->prepare($query)){
+            $stmt->bind_param('i', $eLocID);
+            $stmt->execute();
+            $stmt->bind_result($eLocID, $eLat, $eLong, $eBuild, $eFloor, $eRoom);
+            $stmt->store_result();
+            $stmt->fetch();
+        }
+
         error_log($eName . ' ' . $_GET['Event']);
     } else {
         header("location: ../index.php?error=event not found.");
